0838f82590f49d8efd073975f71ca297be7363de
Leslie Lesauvage <leslie.lesauvage@gmail.com>
2026-01-14 13:46:21 +0100

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,6 @@
 window.APP_CONFIG = {
-    GEMINI_API_KEY: "AIzaSyB9fIensucEZkQrdgm5tKS2Oy3LXykuDH4"
+    GEMINI_API_KEY: "AIzaSyDIV1frLeErdSNKf572Osgrg5Ga5xKnL38"
 };
+
 
 console.log("✅ Configuration chargée");
