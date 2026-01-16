d58bac70df75b036cd5f97170f61ace30e0cc96c
Leslie Lesauvage <leslie.lesauvage@gmail.com>
2026-01-16 11:19:59 +0100

    Changing the API key for my oral presentation.

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,7 @@
 window.APP_CONFIG = {
-    GEMINI_API_KEY: "AIzaSyDIV1frLeErdSNKf572Osgrg5Ga5xKnL38"
+    GEMINI_API_KEY: "AIzaSyA7FAlGEIrBUJvTjtfH2muJFW6fq32y2s8"
 };
 
 
 console.log("✅ Configuration chargée");
+
